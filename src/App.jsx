76d0974d3b572f5410e4b3a200f1cbbf02159247
renaudfournet <fournet.renaud@gmail.com>
2020-10-28 14:01:35 +0100

    first commit, created Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import NewsList from './components/NewsList';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <NewsList />
+      <Navbar />
     </div>
   );
 }
