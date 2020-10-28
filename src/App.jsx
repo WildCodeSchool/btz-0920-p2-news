2424a0ba2d92aac5442c052f94149fbf9e4b4737
Albin Marchand <albin.marchand@gmail.com>
2020-10-28 14:42:57 +0100

    add main logo to header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
+import Header from './components/Header';
 import NewsList from './components/NewsList';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <NewsList />
     </div>
   );
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+import logo from './wildnewsLogo.png';
+
+function Header() {
+  return <img src={logo} alt="Logo" />;
+}
+
+export default Header;
