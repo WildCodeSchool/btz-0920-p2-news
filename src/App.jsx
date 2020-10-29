45248fe180b410e38e14bd80f83ffe21d03d59c8
SALOMON VICTOR <salomonvictor@macbook-pro-de-salomon.home>
2020-10-29 09:24:36 +0100

    added the carousel to the body. This is the first version of the carousel and do not contain anything at the moment

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
 import NewsList from './components/NewsList';
+import MyCarousel from './components/MyCarousel';
 
 function App() {
   return (
     <div className="App">
+      <MyCarousel />
       <NewsList />
     </div>
   );
