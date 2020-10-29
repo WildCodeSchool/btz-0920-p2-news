9d7393c9f669aeec91140771264dbfac9358b6c6
Raphael Lefevre <raph.lefevre@gmail.com>
2020-10-29 08:54:34 +0100

    remove h1 from App.js

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import MainHome from './components/MainHome';
 function App() {
   return (
     <div className="App">
-      <h1>WildNews</h1>
       <MainHome className="App-header" />
     </div>
   );
