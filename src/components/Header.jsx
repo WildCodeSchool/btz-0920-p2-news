ea9c010fd04ce420a35bdf773d998185e8fc455f
Albin Marchand <albin.marchand@gmail.com>
2020-10-30 11:53:58 +0100

    Add trends logo to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import logo from './wildnewsLogo.png';
+import trends from './trends.png';
 import './Header.css';
 
 function Header() {
   return (
     <div className="background">
-      <img className="logo" src={logo} alt="Logo" />;
+      <img className="trends" src={trends} alt="trends" />
+      <img className="logo" src={logo} alt="Logo" />
     </div>
   );
 }
