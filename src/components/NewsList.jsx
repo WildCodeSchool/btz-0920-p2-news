497856ba00d4816b43bb1929fb54468900bc5ff5
Raphael Lefevre <raph.lefevre@gmail.com>
2020-10-31 09:58:43 +0100

    transforming NiwsList class into function with hooks

--- src/components/NewsList.jsx
+++ src/components/NewsList.jsx
@@ -1,65 +1,48 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import Card from './Card';
 import './MainHome.css';
 
 // Ce composant crée une liste de News provenant de l'API
-class NewsList extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      // Il recoit en state initial un tableau vide pendant la phase de construction
-      articlesArray: [],
-    };
-  }
+function NewsList() {
+  const [articlesArray, setArticlesArray] = useState([]);
 
-  // Cette fonction se lance APRES le premier render
-  // Une fois le composant construit, componentDidMount est lancé et fait une requête à l'API
-  // Les datas sont extraites de la réponse de l'API
-  // Data contient un TABLEAU d'articles. On fait un setSate pour remplacer notre tableau vide
-  // par ce nouveau tableau issu de la requete
-  componentDidMount() {
+  useEffect(() => {
+    const country = 'country=fr&';
+    const category = 'category=sports&';
+    const apiTop = 'http://newsapi.org/v2/top-headlines?';
+    const keyAlbin = 'apiKey=e6339d9c525446cc9b6c27d6fbd39e88';
+    const frURL = ` ${apiTop}${country}${category}${keyAlbin} `;
     // Send the request
     axios
-      .get(
-        'http://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=e6339d9c525446cc9b6c27d6fbd39e88'
-      )
+      .get(frURL)
       // Extract the DATA from the received response
       .then((response) => response.data)
       // Use this data to update the state
       .then((data) => {
         // eslint-disable-next-line no-console
         console.log({ data });
-        this.setState({
-          articlesArray: data.articles,
-        });
+        setArticlesArray(data);
       });
-  }
+  });
 
-  // Render a pour rôle de créer les Cards sur chaque article du tableau d'article articleArray
-  // Le premier render s'effectue à vide car dans le state initial, articleArray est vide
-  // Une fois que componentDidMount aura récupéré les articles de l'API et MIS A JOUR le state
-  // alors un nouveau render s'exécute et les News s'affichent
-  render() {
-    const { articlesArray } = this.state;
-    return (
-      <div className="newslist">
-        {articlesArray.map((article) => {
-          return (
-            <Card
-              image={article.urlToImage}
-              title={article.title}
-              url={article.url}
-              date={article.publishedAt}
-              source={article.source.name}
-              description={article.description}
-              key={article.title}
-            />
-          );
-        })}
-      </div>
-    );
-  }
+  return (
+    <div className="newslist">
+      {articlesArray.map((article) => {
+        return (
+          <Card
+            image={article.urlToImage}
+            title={article.title}
+            url={article.url}
+            date={article.publishedAt}
+            source={article.source.name}
+            description={article.description}
+            key={article.title}
+          />
+        );
+      })}
+    </div>
+  );
 }
 
 export default NewsList;
