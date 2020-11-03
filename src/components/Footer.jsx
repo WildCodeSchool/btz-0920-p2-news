79b07067b6bbe4782db5ba58b2e3e3d7e7013423
Albin Marchand <albin.marchand@gmail.com>
2020-11-03 12:25:15 +0100

    Add footer structure

---
+++ src/components/Footer.jsx
@@ -0,0 +1,24 @@
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer className="background">
+      <div className="footerMain">
+        <div className="leftArea">
+          <p>Footer Left Area</p>
+        </div>
+        <div className="middleArea">
+          <p> Footer Middle Area</p>
+        </div>
+        <div className="rightArea">
+          <p> Footer Right Area</p>
+        </div>
+      </div>
+      <div className="baselineArea">
+        <p> Footer Baseline Area</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
