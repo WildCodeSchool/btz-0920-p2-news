7c80d4c06142ad2ebb0d82521fe5d2b85e1d91c2
Albin Marchand <albin.marchand@gmail.com>
2020-11-03 10:04:39 +0100

    fix header displays problems

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,9 +17,6 @@ function Header() {
       </nav>
       <img className="logo" src={logo} alt="Logo" />
     </header>
-    <div className="background">
-      <img className="logo" src={logo} alt="Logo" />
-    </div>
   );
 }
 
