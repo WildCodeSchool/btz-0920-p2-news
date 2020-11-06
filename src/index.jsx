d784f9b2116ebdc97d7ba432e2cff2cd8dc055f8
Albin-Marchand <64652040+Albin-Marchand@users.noreply.github.com>
2020-11-06 10:04:39 +0100

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
