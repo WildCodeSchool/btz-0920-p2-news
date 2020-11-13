553d5c477fd35bf7f4989d5a9c7adef126f50154
Raphael Lefevre <raph.lefevre@gmail.com>
2020-11-13 12:06:38 +0100

    add lorem to articlePage

--- src/components/pages/Article.jsx
+++ src/components/pages/Article.jsx
@@ -26,10 +26,42 @@ const Article = () => {
         <Card>
           <CardImg top width="100%" src={item.urlToImage} alt={item.title} />
           <CardBody>
-            <CardTitle tag="h5">{item.title}</CardTitle>
-            <CardText>{item.content} </CardText>
             <CardText>
-              <small className="text-muted">Last updated 3 mins ago</small>
+              <small className="text-muted">{item.source.name}</small>
+            </CardText>
+            <CardTitle tag="h5">{item.title}</CardTitle>
+            <CardText>
+              {item.content}
+              <p> </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque
+                quo dolor magnam molestiae incidunt labore perspiciatis sequi,
+                officia quis soluta repudiandae deleniti aut accusamus quae? Ex
+                omnis totam asperiores animi blanditiis vero repellat nisi
+                aliquam error aut sunt consequatur architecto voluptatibus nobis
+                dolorum, enim quasi porro. Ex id debitis saepe nostrum dolor?
+                Laboriosam aspernatur necessitatibus, expedita, corporis nostrum
+                tempora asperiores numquam natus hic illo cumque impedit
+                obcaecati incidunt nulla. Quos quo corporis alias doloremque
+                dignissimos quis laborum quibusdam officiis minima.
+              </p>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit
+                cupiditate ad aspernatur, veniam consequuntur dolor numquam
+                inventore ex dolorem quod!
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque
+                quo dolor magnam molestiae incidunt labore perspiciatis sequi,
+                officia quis soluta repudiandae deleniti aut accusamus quae? Ex
+                omnis totam asperiores animi blanditiis vero repellat nisi
+                aliquam error aut sunt consequatur architecto voluptatibus nobis
+                dolorum, enim quasi porro. Ex id debitis saepe nostrum dolor?
+                Laboriosam aspernatur necessitatibus, expedita, corporis nostrum
+                tempora asperiores numquam natus hic illo cumque impedit
+                obcaecati incidunt nulla. Quos quo corporis alias doloremque
+                dignissimos quis laborum quibusdam officiis minima.
+              </p>
             </CardText>
           </CardBody>
         </Card>
