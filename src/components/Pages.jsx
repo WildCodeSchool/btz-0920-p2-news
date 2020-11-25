1872f4720da2c1318cfecedab5f8f89681dd984e
renaudfournet <fournet.renaud@gmail.com>
2020-11-25 12:09:51 +0100

    added api on pages

--- src/components/Pages.jsx
+++ src/components/Pages.jsx
@@ -1,12 +1,30 @@
+import Axios from 'axios';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { apiKey, apiUrl } from '../api';
+// import NewsList from './NewsList';
 
 const Pages = () => {
   const { id } = useParams();
-  return (
-    <div>
-      <h1>{id}</h1>
-    </div>
-  );
+
+  const [categoryArticle, setCategoryArticle] = useState([]);
+
+  useEffect(() => {
+    Axios.get(`${apiUrl}&pageSize=10&category=${id}&apiKey=${apiKey}`).then(
+      (responses) => {
+        const nextState = {
+          category: id,
+          // categoryParam: id,
+          news: responses.data.articles,
+        };
+        // console.log(nextState);
+        setCategoryArticle(nextState);
+      }
+    );
+  }, []);
+  // eslint-disable-next-line no-console
+  console.log(categoryArticle);
+  return <div />;
 };
 
 export default Pages;
