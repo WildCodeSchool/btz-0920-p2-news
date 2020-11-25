689b129af447f94aa5bf882147f31f035c9d1af1
SALOMON VICTOR <victor-salomon@hotmail.fr>
2020-11-25 11:43:05 +0100

    add the keyword search tool

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,12 +1,33 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import Axios from 'axios';
+import { apiUrl, apiKey } from '../api';
+// import NewsList from './NewsList';
 
 const Search = () => {
   const [keyWord, setKeyWord] = useState('');
+  const [keyArticles, setKeyArticles] = useState([]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // console.log(keyWord);
+    // eslint-disable-next-line no-console
+    console.log(keyWord);
   };
+
+  useEffect(() => {
+    Axios.get(`${apiUrl}&q=${keyWord}&apiKey=${apiKey}`).then((res) => {
+      const nextState = {
+        category: 'trump',
+        categoryParam: 'trump',
+        news: res.data.articles,
+      };
+      // eslint-disable-next-line no-console
+      console.log(nextState);
+      setKeyArticles(nextState);
+    });
+  }, [keyWord]);
+
+  // eslint-disable-next-line no-console
+  console.log(keyArticles);
 
   return (
     <>
@@ -21,6 +42,7 @@ const Search = () => {
           <button type="submit">Rechercher</button>
         </label>
       </form>
+      {/* <NewsList articles={keyArticles} /> */}
     </>
   );
 };
